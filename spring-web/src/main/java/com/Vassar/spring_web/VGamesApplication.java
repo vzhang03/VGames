b807abd265f17d16215d03e04bb4bdf899cb789a
vzhang03 <vzhang@vassar.edu>
2024-11-04 12:37:46 -0500

    New spring backend

---
+++ spring-web/src/main/java/com/Vassar/spring_web/VGamesApplication.java
@@ -0,0 +1,13 @@
+package com.Vassar.spring_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VGamesApplication.class, args);
+	}
+
+}
